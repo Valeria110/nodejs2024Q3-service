c6bc8d1b045c3e6550f2fb1699c7d25935ba57af
Valerie Ivashkevich <ivashkevich03@gmail.com>
2024-11-03 17:13:48 +0300

    feat: implement users module

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,11 @@
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  login: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
