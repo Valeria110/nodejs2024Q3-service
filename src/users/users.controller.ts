a7ebad3309c2010821c8fa7b7569067a0cb40910
Valerie Ivashkevich <ivashkevich03@gmail.com>
2024-11-09 20:28:03 +0300

    feat: migrate users module to prisma

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,19 +11,22 @@ import {
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdatePasswordDto } from './dto/update-password.dto';
+import { plainToClass } from 'class-transformer';
+import { User } from './entities/user.entity';
 
 @Controller('user')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  findAll() {
-    return this.usersService.findAll();
+  async findAll() {
+    return await this.usersService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.usersService.findOne(id);
+    const user = this.usersService.findOne(id);
+    return plainToClass(User, user);
   }
 
   @Post()
@@ -32,16 +35,16 @@ export class UsersController {
   }
 
   @Put(':id')
-  update(
+  async update(
     @Param('id') id: string,
     @Body() updatePasswordDto: UpdatePasswordDto,
   ) {
-    return this.usersService.update(id, updatePasswordDto);
+    return await this.usersService.update(id, updatePasswordDto);
   }
 
   @Delete(':id')
   @HttpCode(204)
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(id);
+  async remove(@Param('id') id: string) {
+    return await this.usersService.remove(id);
   }
 }
